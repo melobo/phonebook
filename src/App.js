5e269d64a3f5ea4b257df4bddebe6f081da3bd33
melobo <warchocobo@gmail.com>
2022-03-21 22:06:12 -0600

    Exercises 2.15.-2.18

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
-import Persons from './components/Persons'
-import axios from 'axios'
+import Person from './components/Person'
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -21,29 +21,51 @@ const App = () => {
     const nameArr = persons.map(person => person.name.toLowerCase())
     const hasRepeatedName = nameArr.includes(newName.toLowerCase())
     //Check for name in array
-    if (hasRepeatedName) return window.alert(`${newName} is already added to phonebook`)
-    const phoneObject = {
-      name: newName,
-      number: newNumber,
-      id: persons.length + 1
+    if (hasRepeatedName) {
+      if (!window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) return
+      const person = persons.find(n => n.name.toLowerCase() === newName.toLocaleLowerCase())
+      const changedPerson = {...person, number: newNumber}
+      personService
+        .update(person.id, changedPerson)
+        .then(returnedPerson => {
+          setPersons(persons.map(personMapped => personMapped.id !== person.id ? personMapped : returnedPerson))
+          setNewName('')
+          setNewNumber('')
+        })
+    }else {
+      const personObject = {
+        name: newName,
+        number: newNumber,
+      }
+      personService
+        .create(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNumber('')
+        })
     }
-    
-    setPersons(persons.concat(phoneObject))
-    setNewName('')
-    setNewNumber('')
   }
 
   const filteredPersons = !searchName
     ? persons  
     : persons.filter(person => person.name.toLowerCase().includes(searchName.toLowerCase()))
 
+  const deletePerson = deletedPerson => {
+    if (!window.confirm(`Delete ${deletedPerson.name}?`)) return
+    personService
+      .del(deletedPerson.id)
+      .then(personDeleted => {
+        if (!personDeleted) return
+        setPersons(persons.filter(person => person.id !== deletedPerson.id))
+      })
+  }
+
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, [])
 
@@ -63,7 +85,13 @@ const App = () => {
         handleNumberChange={handleNumberChange}
       />
       <h3>Numbers</h3>
-      <Persons filteredPersons={filteredPersons}/>
+      {filteredPersons.map(person => (
+        <Person
+          key={person.id}
+          person={person}
+          handleDelete={() => deletePerson(person)}
+        />
+      ))}
     </div>
   )
 }
